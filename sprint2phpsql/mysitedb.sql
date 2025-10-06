fcae67437bb24bede3f1565f838ee6291fdfae26
Elena <efernandezf25@fpcoruna.afundacion.org>
2025-10-06 11:53:07 +0200

    Sprint2 - Crear base de datos canciones

---
+++ sprint2phpsql/mysitedb.sql
@@ -0,0 +1,137 @@
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19-11.8.3-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: mysitedb
+-- ------------------------------------------------------
+-- Server version	11.8.3-MariaDB-0+deb13u1 from Debian
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
+
+--
+-- Table structure for table `tCanciones`
+--
+
+DROP TABLE IF EXISTS `tCanciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tCanciones` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `url_imagen` varchar(200) DEFAULT NULL,
+  `artista` varchar(100) DEFAULT NULL,
+  `album` varchar(100) DEFAULT NULL,
+  `ano` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tCanciones`
+--
+
+LOCK TABLES `tCanciones` WRITE;
+/*!40000 ALTER TABLE `tCanciones` DISABLE KEYS */;
+set autocommit=0;
+INSERT INTO `tCanciones` VALUES
+(1,'all-american bitch','https://i.scdn.co/image/ab67616d0000b2734063d624ebf8ff67bc3701ee','Olivia Rodrigo','GUTS(spilled)',2024),
+(2,'So Easy (To Fall In Love)','https://m.media-amazon.com/images/I/91ogA7DN6jL._UF894,1000_QL80_.jpg','Olivia Dean','The Art of Loving',2025),
+(3,'Super Graphic Ultra Modern Girl','https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/202409/09/00105112778047____1__640x640.jpg','Chapell Roan','The Rise and Fall of a Midwest Princess',2023),
+(4,'Sinner','https://m.media-amazon.com/images/I/91MRLOrmk+L._UF1000,1000_QL80_.jpg','The Last Dinner Party','Prelude to Ecstasy',2024),
+(5,'Hallucinate','https://m.media-amazon.com/images/I/71VQFsqlPJL.jpg','Dua Lipa','Future Nostalgia',2020);
+/*!40000 ALTER TABLE `tCanciones` ENABLE KEYS */;
+UNLOCK TABLES;
+commit;
+
+--
+-- Table structure for table `tComentarios`
+--
+
+DROP TABLE IF EXISTS `tComentarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tComentarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `comentario` varchar(2000) DEFAULT NULL,
+  `usuario_id` int(11) DEFAULT NULL,
+  `cancion_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuario_id` (`usuario_id`),
+  KEY `cancion_id` (`cancion_id`),
+  CONSTRAINT `tComentarios_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `tUsuarios` (`id`),
+  CONSTRAINT `tComentarios_ibfk_2` FOREIGN KEY (`cancion_id`) REFERENCES `tCanciones` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tComentarios`
+--
+
+LOCK TABLES `tComentarios` WRITE;
+/*!40000 ALTER TABLE `tComentarios` DISABLE KEYS */;
+set autocommit=0;
+INSERT INTO `tComentarios` VALUES
+(1,'Olivia transmite mucha autenticidad.',1,1),
+(2,'Esta canción me pone de buen humor.',2,2),
+(3,'El ritmo es tan pegadizo que no puedo dejar de bailarlo.',3,3),
+(4,'Me encanta, suena como una historia contada en música.',4,4),
+(5,'Puro pop.',5,5),
+(6,'La producción es suave pero elegante, perfecta para una tarde tranquila.',3,2),
+(7,'Chapell Roan tiene un estilo único.',4,3);
+/*!40000 ALTER TABLE `tComentarios` ENABLE KEYS */;
+UNLOCK TABLES;
+commit;
+
+--
+-- Table structure for table `tUsuarios`
+--
+
+DROP TABLE IF EXISTS `tUsuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tUsuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `apellidos` varchar(100) DEFAULT NULL,
+  `email` varchar(200) DEFAULT NULL,
+  `contraseña` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tUsuarios`
+--
+
+LOCK TABLES `tUsuarios` WRITE;
+/*!40000 ALTER TABLE `tUsuarios` DISABLE KEYS */;
+set autocommit=0;
+INSERT INTO `tUsuarios` VALUES
+(1,'Lucas','Martínez','lucas.martinez@example.com','Gj7$kP2r!vQ9'),
+(2,'María','García','maria.garcia@example.com','R8m#tY4p@zL1'),
+(3,'Sofía','Ramos','sofia.ramos@example.com','Tq3%Hb9w&Xc2'),
+(4,'Diego','Santos','diego.santos@example.com','Vz6^Np5b*Lm8'),
+(5,'Ana','López','ana.lopez@example.com','Yp2!Qs7d#Wf6');
+/*!40000 ALTER TABLE `tUsuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+commit;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
+
+-- Dump completed on 2025-10-06 11:50:40
