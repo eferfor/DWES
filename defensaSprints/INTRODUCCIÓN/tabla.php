1a30912f32f098b797fbc0792c34433901b48188
Elena <efernandezf25@fpcoruna.afundacion.org>
2025-11-05 13:32:56 +0100

    Ejercicio tabla.php

--- defensaSprints/INTRODUCCIÓN/tabla.php
+++ defensaSprints/INTRODUCCIÓN/tabla.php
@@ -5,13 +5,17 @@
       <tr><th>Expresión</th><th>Resultado</th></tr>
       <?php
       /*Cambia el for por un while*/
-        for ($i = 1; $i <= 10; $i = $i + 1) {
+        $res = 0;
+        $i = 1;
+        while($res < 70){
           $res = 7 * $i;
           echo "<tr>";
           echo "<td>7 x " . $i . "</td>";
           echo "<td>" . $res . "</td>";
           echo "</tr>";
+          $i++;
         }
+
       ?>
     </table>
   </body>
